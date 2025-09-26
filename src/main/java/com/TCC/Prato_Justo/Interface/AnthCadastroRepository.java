64a15e9aad17d8180544d0420d6522008857f547
leafarture <rafahue.rafahue@gmail.com>
2025-09-26 16:57:36 -0300

    atualizando banco de dados

--- src/main/java/com/TCC/Prato_Justo/Interface/AnthCadastroRepository.java
+++ src/main/java/com/TCC/Prato_Justo/Interface/AnthCadastroRepository.java
@@ -2,12 +2,10 @@ package com.TCC.Prato_Justo.Interface;
 
 import com.TCC.Prato_Justo.Model.Cadastro;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface AnthCadastroRepository extends JpaRepository<Cadastro, Long> {
-    Optional<Cadastro> findByNomeAndSenhaHashAndEmail(String nome, String senhaHash, String email);
+    Optional<Cadastro> findByUsernameAndPasswordAndEmail(String username, String password, String email);
     Optional<Cadastro> findByEmail(String email);
 }
