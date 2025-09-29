9233ccc88a00c442f85b0f88ccfc333812a8b621
leafarture <rafahue.rafahue@gmail.com>
2025-09-28 22:50:44 -0300

    atualizando banco de dados

--- src/main/java/com/TCC/Prato_Justo/Controller/AutchCadastroController.java
+++ src/main/java/com/TCC/Prato_Justo/Controller/AutchCadastroController.java
@@ -18,15 +18,15 @@ public class AutchCadastroController {
     }
 
     @PostMapping("registro")
-    public Cadastro registro(@RequestBody Cadastro cadastro){
-        return cadastroService.cadastrarNovoUsusario(cadastro);
+    public Cadastro registro(@RequestBody CadastroRequest request){
+        return cadastroService.cadastrarNovoUsuario(request);
     }
 
     @PostMapping("/cadastros")
     public String cadastros(@RequestBody Cadastro cadastro){
-        Cadastro cad = cadastroService.fazerCadastro(cadastro.getUsername(), cadastro.getEmail(), cadastro.getPassword());
+        Cadastro cad = cadastroService.fazerCadastro(cadastro.getNome(), cadastro.getEmail(), cadastro.getPassword());
         if(cad != null){
-            return "Cadastro Realizado" + cad.getUsername() + "!!!!!";
+            return "Cadastro Realizado" + cad.getNome() + "!!!!!";
 
         }
         return "Usuario ou senha ou email errada !!!!";
@@ -59,6 +59,82 @@ public class AutchCadastroController {
 
         public void setPassword(String password) {
             this.password = password;
+        }
+    }
+
+    public static class CadastroRequest {
+        private String username;
+        private String email;
+        private String password;
+        private String perfil;
+        private String telefone;
+        private String enderecoCompleto;
+        private Double latitude;
+        private Double longitude;
+
+        // Getters e Setters
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        public String getPerfil() {
+            return perfil;
+        }
+
+        public void setPerfil(String perfil) {
+            this.perfil = perfil;
+        }
+
+        public String getTelefone() {
+            return telefone;
+        }
+
+        public void setTelefone(String telefone) {
+            this.telefone = telefone;
+        }
+
+        public String getEnderecoCompleto() {
+            return enderecoCompleto;
+        }
+
+        public void setEnderecoCompleto(String enderecoCompleto) {
+            this.enderecoCompleto = enderecoCompleto;
+        }
+
+        public Double getLatitude() {
+            return latitude;
+        }
+
+        public void setLatitude(Double latitude) {
+            this.latitude = latitude;
+        }
+
+        public Double getLongitude() {
+            return longitude;
+        }
+
+        public void setLongitude(Double longitude) {
+            this.longitude = longitude;
         }
     }
 }
